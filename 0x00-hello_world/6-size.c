b1d38728016ed38f00ac98943afb0a4210870e24
Wilbrord <bwilbrord@gmail.com>
2022-09-15 06:01:48 -0700

    correct error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ int main(void)
 	long li;
 	long long lli;
 	float f;
-	printf("Size of a char: %c byte(s)\n", sizeof(c));
-	printf("Size of an int: %d byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("size of a long long int:%ld byte(s)\n", sizeof(lli));
-	printf("Size of a float: %f byte(s)\n," sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n",(unsigned long) sizeof(li));
+	printf("size of a long long int:%lu byte(s)\n",(unsigned long) sizeof(lli));
+	printf("Size of a float: %lu byte(s)\n," (unsigned long)sizeof(f));
 	return(0);
 }
 
