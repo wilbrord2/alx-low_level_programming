417280fb343fda5b3fe64365f852af87cec9b500
Wilbrord <bwilbrord@gmail.com>
2022-09-19 06:50:26 -0700

    program to display combination of thre numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,14 @@
 int main(void)
 {
 	int i;
-	for(i = 0; i <= 90; i++)
+	for(i= 1; i <= 90; i++)
 	{
 		putchar((i / 10) + '0');
 		putchar((i % 10) + '0');
-	}
 	if (i  != 89){
 		putchar(',');
 		putchar(' ');
-	}
+	}}
 	putchar('\n');
 	return (0);
 }
