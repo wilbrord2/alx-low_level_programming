bf0ab6ef5db893b6f64dc4a878ab31c3025708e3
Wilbrord <bwilbrord@gmail.com>
2022-09-19 06:08:55 -0700

    add numberof bass 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - function
+ *
+ * Return 0:
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
